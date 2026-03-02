4b4ebbc65c4cfed4c6df2fc9bc43a684017de869
Jordi Gerona <jordi@donky.org>
2026-03-02 15:31:33 +0100

    fix: bump vanniktech maven-publish plugin to 0.36.0 for Gradle 9 compatibility
    SonatypeHost was removed in 0.34.0; publishToMavenCentral() no longer
    takes a parameter.

--- build-logic/src/main/kotlin/krat.maven-publish.gradle.kts
+++ build-logic/src/main/kotlin/krat.maven-publish.gradle.kts
@@ -1,6 +1,5 @@
 import com.vanniktech.maven.publish.JavadocJar
 import com.vanniktech.maven.publish.KotlinJvm
-import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
   id("com.vanniktech.maven.publish")
@@ -10,7 +9,7 @@ plugins {
 version = findProperty("version")?.toString()?.takeIf { it != "unspecified" } ?: "0.0.0-SNAPSHOT"
 
 mavenPublishing {
-  publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+  publishToMavenCentral()
   signAllPublications()
 
   configure(KotlinJvm(
