b9b4d9d4eb5b766b19c374bd61ddb44f2e108401
Jordi Gerona <jordi@donky.org>
2026-06-05 12:05:53 +0200

    feat(testlib): Add JSON request body helper
    Ktor tests often need the same ceremony to submit raw JSON request bodies: set the application/json content type and trim multiline fixtures. Keeping that helper in application test code fragments a reusable testing concern, while krat-pack-testlib already owns HTTP test utilities.
    
    Changes:
    - Add HttpRequestBuilder.setJsonBody for application/json request bodies
    - Trim multiline request fixtures before sending them
    - Verify the helper against a Ktor test application

---
+++ krat-pack-testlib/src/main/kotlin/com/jordi9/krat/pack/test/JsonRequest.kt
@@ -0,0 +1,11 @@
+package com.jordi9.krat.pack.test
+
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+
+fun HttpRequestBuilder.setJsonBody(body: String) {
+  contentType(ContentType.Application.Json)
+  setBody(body.trimIndent())
+}
---
+++ krat-pack-testlib/src/test/kotlin/com/jordi9/krat/pack/test/JsonRequestShould.kt
@@ -0,0 +1,52 @@
+package com.jordi9.krat.pack.test
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.ktor.client.request.post
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.request.contentType
+import io.ktor.server.request.receiveText
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.post
+import io.ktor.server.routing.routing
+import io.ktor.server.testing.testApplication
+
+class JsonRequestShould : StringSpec({
+
+  "send trimmed json body with application json content type" {
+    var receivedContentType: ContentType? = null
+    var receivedBody: String? = null
+
+    testApplication {
+      application {
+        routing {
+          post("/items") {
+            receivedContentType = call.request.contentType()
+            receivedBody = call.receiveText()
+            call.respondText("ok")
+          }
+        }
+      }
+
+      val response = client.post("/items") {
+        setJsonBody(
+          """
+            {
+              "name": "Cash"
+            }
+          """
+        )
+      }
+
+      response.status shouldBe HttpStatusCode.OK
+      receivedContentType shouldBe ContentType.Application.Json
+      receivedBody shouldBe
+        """
+          {
+            "name": "Cash"
+          }
+        """.trimIndent()
+    }
+  }
+})
